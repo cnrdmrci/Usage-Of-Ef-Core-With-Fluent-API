7e3352c9c9f17bd2438d32f3f1a3412da8be8859
Caner Demirci <canerdemirci.mail@gmail.com>
2020-01-11 01:39:27 +0300

    One to one relational classes created

---
+++ OneToOne/ContactInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EntityFrameworkCoreExamples.OneToOne
+{
+    public class ContactInfo
+    {
+        public int ContactInfoId { get; set; }
+        public string City { get; set; }
+        public string Phone { get; set; }
+
+        public int WorkerId { get; set; }
+        public virtual Worker Worker { get; set; }
+    }
+}
---
+++ OneToOne/Worker.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EntityFrameworkCoreExamples.OneToOne
+{
+    public class Worker
+    {
+        public int WorkerId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public virtual ContactInfo ContactInfo { get; set; }
+    }
+}
