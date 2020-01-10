a75139f8cb830131e71d767e91d144dffddca2e6
cnrdmrci <cnrdmrci@hotmail.com>
2020-01-11 00:46:40 +0300

    Project created

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UsageOfEfCoreWithFluentApi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Program Created!");
+        }
+    }
+}
