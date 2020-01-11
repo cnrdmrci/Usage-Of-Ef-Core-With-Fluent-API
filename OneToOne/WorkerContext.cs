d01f158fb3111964d56a8048b91ab444c0dab382
Caner Demirci <canerdemirci.mail@gmail.com>
2020-01-11 16:29:32 +0300

    added worker DbContext

---
+++ OneToOne/WorkerContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace EntityFrameworkCoreExamples.OneToOne
+{
+    public class WorkerContext : DbContext
+    {
+        public DbSet<Worker> Worker { get; set; }
+        public DbSet<ContactInfo> ContactInfo { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=.\SQLEXPRESS;initial catalog=[DATABASE];user id=[USERNAME];password=[PASSWORD];");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new WorkerConfiguration());
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
